a80fb2e6f68fc6084cceae148f181dfadd9889f9
windupbirdjdt <114854180+windupbirdjdt@users.noreply.github.com>
2022-10-02 10:39:16 +0100

    Update views.py

--- other/views.py
+++ other/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def simple_view(request):
-    return HttpResponse('Hello James')
+    return HttpResponse('Hello Sexy!! Love u sexy and Team Bom Bom!!')
 
 # Create your views here.
