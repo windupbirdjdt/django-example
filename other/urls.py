60c1d7b8d27f864ef4d5c73ab27d6bcd5ed20625
windupbirdjdt <114854180+windupbirdjdt@users.noreply.github.com>
2022-10-02 16:31:11 +0100

    Testing About

--- other/urls.py
+++ other/urls.py
@@ -3,5 +3,6 @@ from .import views
 
 urlpatterns = [
     path('',views.simple_view)
+    path('about/',AboutPage.as_view(),name='about')
 ]
 
--- other/views.py
+++ other/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 def simple_view(request):
     return HttpResponse('Hello Sexy!! Love u sexy and Team Bom Bom!!')
 
 # Create your views here.
+
+class AboutPage(TemplateView):
+    template_name = "about.html"
